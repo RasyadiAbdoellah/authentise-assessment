4bb3751fcc08dabfdfa2860653bc1b3ad188ba31
Ras Abdoellah <me@rasyadi.dev>
2022-02-06 00:30:04 -0500

    Improve styling & add Loader to Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,18 @@
 import React from "react";
 
+import Loader from "./Loader";
 import { useGet } from "../utils";
 
 export default function Card({ breed, remover = () => {} }) {
-  const { data: img, fetchData } = useGet(`breed/${breed}/images/random/1`);
+  const { data: img, reqStatus, fetchData } = useGet(`breed/${breed}/images/random/1`);
   return (
     <div className="card">
       <h1>{breed}</h1>
       <button onClick={() => fetchData()}>Get new image</button>
       <button onClick={() => remover()}>Remove</button>
-      <img src={img} />
+      <Loader status={reqStatus}>
+        <img src={img} />
+      </Loader>
     </div>
   );
 }
